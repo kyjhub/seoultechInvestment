906117dffd77aafbf81911a66edfa632580cc501
Yong-jun Kwon <harryjun43@gmail.com>
2024-03-20 17:00:57 +0900

    connect database with mysql

--- src/main/java/com/example/seoultechInvestment/entity/Member.java
+++ src/main/java/com/example/seoultechInvestment/entity/Member.java
@@ -1,17 +1,20 @@
 package com.example.seoultechInvestment.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Member {
     @Id
     @NotBlank(message ="학번입력은 필수입니다.")
+    @Column(unique = true, length = 8)
     Long stId;
-    @NotBlank(message ="학번입력은 필수입니다.")
+    @NotBlank(message ="이름은 필수입니다.")
     String name;
-    @NotBlank(message ="학번입력은 필수입니다.")
+    @NotBlank(message ="학과는 필수입니다.")
     String Department;
     String stGalleryNickname; //cam empty
 }
--- src/main/java/com/example/seoultechInvestment/entity/RecommendedList.java
+++ src/main/java/com/example/seoultechInvestment/entity/RecommendedList.java
@@ -3,6 +3,7 @@ package com.example.seoultechInvestment.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 public class RecommendedList {
     @Id
     @NotBlank(message = "추천등록날짜가 입력되지 않았습니다.") //근데 이건 자동으로 등록될거라 굳이 검증 안해도 되는데 통신 오류로 발생할 수 있으니까
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     LocalDateTime localDateTime;
     @NotBlank(message = "추천등록날짜가 입력되지 않았습니다.")
     String tickerName; //티커명은 트레이딩뷰 기준
