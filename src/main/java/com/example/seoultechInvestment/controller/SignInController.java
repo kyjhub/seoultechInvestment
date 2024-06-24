92e868fd3310974b3628125991acf86ddb39af88
Yong-jun Kwon <harryjun43@gmail.com>
2024-06-24 10:11:15 +0900

    병합 문제를 해결하기위한 커밋

--- src/main/java/com/example/seoultechInvestment/controller/SignInController.java
+++ src/main/java/com/example/seoultechInvestment/controller/SignInController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 
+//관리자 계정으로 로그인하면 다른 화면으로 들어가도록
+//일반회원과 관리자가 로그인하면 넘어가는 페이지가 다른 걸로
 @Controller
 public class SignInController {
     @PostMapping("/login")
