ad4fac9f8faa4d3b92d8f4ec609a06b2fdb09b7f
kyjhub <harryjun43@gmail.com>
2024-06-30 19:36:22 +0900

    InitDB 클래스 완성

--- src/main/java/com/example/seoultechInvestment/InitDB.java
+++ src/main/java/com/example/seoultechInvestment/InitDB.java
@@ -2,18 +2,21 @@ package com.example.seoultechInvestment;
 
 import com.example.seoultechInvestment.entity.Member;
 import com.example.seoultechInvestment.repository.MemberRepository;
-import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class DbInit {
+public class InitDB {
     private final MemberRepository memberRepository;
 
-    @PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
     public void init(){
         log.info("init method 실행");
         Member initMember = Member.builder().stId(21101165L).Department("컴퓨터공학과").password("1234")
