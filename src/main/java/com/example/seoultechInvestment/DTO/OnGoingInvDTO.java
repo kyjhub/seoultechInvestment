8bd75ca951e265a1c53fc544afc1335316899fb9
Yong-jun Kwon <harryjun43@gmail.com>
2024-07-19 16:54:00 +0900

    stInvestmentHome.html 완성

--- src/main/java/com/example/seoultechInvestment/DTO/OnGoingInvDTO.java
+++ src/main/java/com/example/seoultechInvestment/DTO/OnGoingInvDTO.java
@@ -15,11 +15,11 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OnGoingInvDTO {
-    @NotBlank(message = "종목명이 입력되지 않았습니다.")
-    private String tickerName;
     @NotNull(message = "추천등록날짜가 입력되지 않았습니다.") //근데 이건 자동으로 등록될거라 굳이 검증 안해도 되는데 통신 오류로 발생할 수 있으니까
     @DateTimeFormat(pattern = "yyyy-mm-dd") //등록할 때 localDate.now
     private LocalDate enrollDate;
+    @NotBlank(message = "종목명이 입력되지 않았습니다.")
+    private String tickerName;
     @NotNull(message = "tp값이 입력되지 않았습니다.")
     private double tp;
     @NotNull
