489c8012ce82e17d5781d482b6d47ee3f3c08a43
Yong-jun Kwon <harryjun43@gmail.com>
2024-10-30 14:20:54 +0900

    보안을 위해 텔레그램 개인정보 전용 설저파일 만들고 gitgnore파일에 추가

--- src/main/java/com/example/seoultechInvestment/config/TelegramConfig.java
+++ src/main/java/com/example/seoultechInvestment/config/TelegramConfig.java
@@ -1,8 +1,16 @@
 package com.example.seoultechInvestment.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application-security.yml")
 public class TelegramConfig {
-
+    @Value("${telegramBot.token}")
+    String Token;
+    @Value("${telegramBot.chatId}")
+    String chatId;
+    String text;
 }
