cc42cfc2752e9b65fab5dd39e526363529aec213
Yong-jun Kwon <harryjun43@gmail.com>
2024-11-05 16:51:16 +0900

    다른 yml파일 참조 에러 해결

--- src/main/java/com/example/seoultechInvestment/config/EmailConfig.java
+++ src/main/java/com/example/seoultechInvestment/config/EmailConfig.java
@@ -1,6 +1,7 @@
 package com.example.seoultechInvestment.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import java.util.Properties;
 @Configuration
 @PropertySource(value = "classpath:/application-security.yml")
 @Getter
+@Setter
 public class EmailConfig {
 
     @Value("${naver-mail.id}")
